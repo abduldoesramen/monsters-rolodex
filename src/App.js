34a7eb60159833961c9a70d9493b257ea2cf29dd
abdul <abdulrehman133782@gmail.com>
2023-01-09 10:42:21 +1100

    added keys

--- src/App.js
+++ src/App.js
@@ -11,15 +11,19 @@ class App extends Component {
       monsters: [
         {
           name: "Linda",
+          id: "123a",
         },
         {
           name: "Frank",
+          id: "123b",
         },
         {
           name: "Jacky",
+          id: "123c",
         },
         {
           name: "Andrei",
+          id: "123d",
         },
       ],
     };
@@ -29,7 +33,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
     );
