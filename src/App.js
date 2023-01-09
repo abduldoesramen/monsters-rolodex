041dbdc5f7207c059edd52badeacd3d4d6e9605c
abdul <abdulrehman133782@gmail.com>
2023-01-09 12:19:57 +1100

    added readability for code

--- src/App.js
+++ src/App.js
@@ -32,12 +32,22 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   // 2. Determines what to show (Initial UI)
   render() {
     console.log("render");
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -46,12 +56,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monster"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
