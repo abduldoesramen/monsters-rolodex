d5cac91538214324bbd67bef825d98e031093ecc
abdul <abdulrehman133782@gmail.com>
2023-01-10 15:52:57 +1100

    finished styling

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">The Amazing Monsters Rolodex!</h1>
         <SearchBox
           className="monsters-search-box"
           onChangeHandler={onSearchChange}
