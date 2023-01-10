a2519721799873b0f7b18af8b5159eff73a3830d
abdul <abdulrehman133782@gmail.com>
2023-01-10 15:40:48 +1100

    changed cards into their own component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 
+import Card from '../card/card.component';
 import './card-list.styles.css';
 
 class CardList extends Component {
@@ -11,11 +12,7 @@ class CardList extends Component {
                 {monsters.map((monster) => {
                     const { name, email, id } = monster;
                     return (
-                    <div className='card-container' key={id}>
-                        <img alt={`monster ${name}`} src={`https://robohash.org/${id+Math.floor(Math.random()*100)}?set=set2&size=180x180`}/>
-                        <h2>{name}</h2>
-                        <p>{email}</p>
-                    </div>
+                        <Card monster={monster}/>
                 )})}
             </div>
         )
