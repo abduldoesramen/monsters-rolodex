d1857b878a58023c141e31b07dc4387633a9cc97
abdul <abdulrehman133782@gmail.com>
2023-01-10 16:27:22 +1100

    stopped random avatar gen

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -7,7 +7,7 @@ class Card extends Component {
         const {id, name, email} = this.props.monster;
         return (
             <div className='card-container' key={id}>
-                <img alt={`monster ${name}`} src={`https://robohash.org/${id + Math.floor(Math.random() * 100)}?set=set2&size=180x180`}/>
+                <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
                 <h2>{name}</h2>
                 <p>{email}</p>
             </div>
