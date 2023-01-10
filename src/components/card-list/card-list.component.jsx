bdc02def27127ff105ffb0d73bf55cb840cc8be0
abdul <abdulrehman133782@gmail.com>
2023-01-10 12:59:53 +1100

    finished styling app cards

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,18 @@
 import { Component } from 'react';
 
+import './card-list.styles.css';
+
 class CardList extends Component {
     render() {
         const { monsters } = this.props;
-        
+
         return (
-            <div>
+            <div className='card-list'>
                 {monsters.map((monster) => {
                     const { name, email, id } = monster;
                     return (
                     <div className='card-container' key={id}>
-                        <img alt={`monster ${name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}/>
+                        <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
                         <h2>{name}</h2>
                         <p>{email}</p>
                     </div>
